179f380ed8a8b9309f89cfd2953806574e3d54cf
Athou <jeremiepanzer@gmail.com>
2016-12-16 12:11:24 +0100

    option panel to configure output

--- main.lua
+++ main.lua
@@ -9,9 +9,9 @@ function filter(self, event, message, user, ...)
 			local _, _, Color, Ltype, Id, Enchant, Gem1, Gem2, Gem3, Gem4, Suffix, Unique, LinkLvl, Name = string.find(itemLink, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*):?(%-?%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?")
 			
 			local attrs = {}
-			if (itemSubType ~= nil) then table.insert(attrs, itemSubType) end
-			if (itemEquipLoc ~= nil and _G[itemEquipLoc] ~= nil) then table.insert(attrs, _G[itemEquipLoc]) end
-			if (iLevel ~= nil) then table.insert(attrs, iLevel) end
+			if (SavedData.show_subtype and itemSubType ~= nil) then table.insert(attrs, itemSubType) end
+			if (SavedData.show_equiploc and itemEquipLoc ~= nil and _G[itemEquipLoc] ~= nil) then table.insert(attrs, _G[itemEquipLoc]) end
+			if (SavedData.show_ilevel and iLevel ~= nil) then table.insert(attrs, iLevel) end
 			
 			local newItemName = itemName.." ("..table.concat(attrs, " ")..")"
 			local newLink = "|cff"..Color.."|H"..itemString.."|h["..newItemName.."]|h|r"
@@ -24,7 +24,7 @@ end
 
 -- Inhibit Regular Expression magic characters ^$()%.[]*+-?)
 function escapeSearchString(str)
-    return str:gsub("(%W)","%%%1")
+	return str:gsub("(%W)","%%%1")
 end
 
 local function eventHandler(self, event, ...)
@@ -46,6 +46,36 @@ local function eventHandler(self, event, ...)
 	ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", filter);
 	ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", filter)
 	ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", filter);
+	
+	if (SavedData == nil) then 
+		SavedData = {}
+		SavedData.show_subtype = true
+		SavedData.show_equiploc = true
+		SavedData.show_ilevel = true
+	end
+	
+	local panel = CreateFrame("Frame", "OptionsPanel", UIParent)
+	panel.name = "ItemLinkLevel"
+
+	local subtypeCheckBox = CreateFrame("CheckButton", "subtypeCheckBox", panel, "UICheckButtonTemplate")
+	subtypeCheckBox:SetPoint("TOPLEFT",10, -30)
+	subtypeCheckBox:SetChecked(SavedData.show_subtype)
+	_G[subtypeCheckBox:GetName().."Text"]:SetText("Display armor/weapon type (Plate, Leather, ...)")
+	subtypeCheckBox:SetScript("OnClick", function(self) SavedData.show_subtype = self:GetChecked() end)
+
+	local equipLocCheckbox = CreateFrame("CheckButton", "equipLocCheckbox", panel, "UICheckButtonTemplate")
+	equipLocCheckbox:SetPoint("TOPLEFT",10, -60)
+	equipLocCheckbox:SetChecked(SavedData.show_equiploc)
+	_G[equipLocCheckbox:GetName().."Text"]:SetText("Display equip location (Head, Trinket, ...)")
+	equipLocCheckbox:SetScript("OnClick", function(self) SavedData.show_equiploc = self:GetChecked() end)
+
+	local iLevelCheckbox = CreateFrame("CheckButton", "iLevelCheckbox", panel, "UICheckButtonTemplate")
+	iLevelCheckbox:SetPoint("TOPLEFT",10, -90)
+	iLevelCheckbox:SetChecked(SavedData.show_ilevel)
+	_G[iLevelCheckbox:GetName().."Text"]:SetText("Display item level")
+	iLevelCheckbox:SetScript("OnClick", function(self) SavedData.show_ilevel = self:GetChecked() end)
+
+	InterfaceOptions_AddCategory(panel)
 end
 frame:SetScript("OnEvent", eventHandler);
 
