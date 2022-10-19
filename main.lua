b6f904f042556b1280151f79effb30a4a389b74c
Athou <jeremiepanzer@gmail.com>
2022-10-19 08:13:22 +0200

    format file using https://github.com/sumneko/lua-language-server/wiki/Formatter

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ local socketTooltip
 
 -- Inhibit Regular Expression magic characters ^$()%.[]*+-?)
 local function EscapeSearchString(str)
-	return str:gsub("(%W)","%%%1")
+	return str:gsub("(%W)", "%%%1")
 end
 
 -- function borrowed from PersonalLootHelper
@@ -35,7 +35,7 @@ end
 -- function borrowed from PersonalLootHelper
 local function PLH_GetRelicType(item)
 	local relicType = nil
-	
+
 	if item ~= nil then
 		tooltip = tooltip or CreateEmptyTooltip()
 		tooltip:ClearLines()
@@ -47,50 +47,50 @@ local function PLH_GetRelicType(item)
 		while not relicType and tooltip.leftside[index] do
 			t = tooltip.leftside[index]:GetText()
 			if t ~= nil then
-				relicType = t:match(PLH_RELIC_TOOLTIP_TYPE_PATTERN)				
+				relicType = t:match(PLH_RELIC_TOOLTIP_TYPE_PATTERN)
 			end
 			index = index + 1
 		end
 
 		tooltip:Hide()
 	end
-	
+
 	return relicType
 end
 
 -- function borrowed from PersonalLootHelper
 local function PLH_GetRealILVL(item)
 	local realILVL = nil
-	
+
 	if item ~= nil then
 		tooltip = tooltip or CreateEmptyTooltip()
 		tooltip:ClearLines()
 		tooltip:SetHyperlink(item)
 		local t = tooltip.leftside[2]:GetText()
 		if t ~= nil then
---			realILVL = t:match('Item Level (%d+)')
+			-- realILVL = t:match('Item Level (%d+)')
 			realILVL = t:match(PLH_ITEM_LEVEL_PATTERN)
 		end
 		-- ilvl can be in the 2nd or 3rd line dependng on the tooltip; if we didn't find it in 2nd, try 3rd
 		if realILVL == nil then
 			t = tooltip.leftside[3]:GetText()
 			if t ~= nil then
---				realILVL = t:match('Item Level (%d+)')
+				-- realILVL = t:match('Item Level (%d+)')
 				realILVL = t:match(PLH_ITEM_LEVEL_PATTERN)
 			end
 		end
 		tooltip:Hide()
-		
+
 		-- if realILVL is still nil, we couldn't find it in the tooltip - try grabbing it from getItemInfo, even though
 		--   that doesn't return upgrade levels
 		if realILVL == nil then
 			_, _, _, realILVL, _, _, _, _, _, _, _ = GetItemInfo(item)
 		end
 	end
-	
+
 	if realILVL == nil then
 		return 0
-	else		
+	else
 		return tonumber(realILVL)
 	end
 end
@@ -101,14 +101,14 @@ local function ItemHasSockets(itemLink)
 	socketTooltip:SetOwner(UIParent, 'ANCHOR_NONE')
 	socketTooltip:ClearLines()
 	for i = 1, 30 do
-		local texture = _G[socketTooltip:GetName().."Texture"..i]
+		local texture = _G[socketTooltip:GetName() .. "Texture" .. i]
 		if texture then
 			texture:SetTexture(nil)
 		end
 	end
 	socketTooltip:SetHyperlink(itemLink)
 	for i = 1, 30 do
-		local texture = _G[socketTooltip:GetName().."Texture"..i]
+		local texture = _G[socketTooltip:GetName() .. "Texture" .. i]
 		local textureName = texture and texture:GetTexture()
 
 		if textureName then
@@ -122,22 +122,24 @@ end
 local function Filter(self, event, message, user, ...)
 	for itemLink in message:gmatch("|%x+|Hitem:.-|h.-|h|r") do
 		local itemName, _, quality, _, _, _, itemSubType, _, itemEquipLoc, _, _, itemClassId, itemSubClassId = GetItemInfo(itemLink)
-		if (quality ~= nil and quality >= SavedData.trigger_quality and (itemClassId == Enum.ItemClass.Weapon or itemClassId == Enum.ItemClass.Gem or itemClassId == Enum.ItemClass.Armor)) then
+		if (
+			quality ~= nil and quality >= SavedData.trigger_quality and
+				(itemClassId == Enum.ItemClass.Weapon or itemClassId == Enum.ItemClass.Gem or itemClassId == Enum.ItemClass.Armor)) then
 			local itemString = string.match(itemLink, "item[%-?%d:]+")
 			local color = ITEM_QUALITY_COLORS[quality].hex
 			local iLevel = PLH_GetRealILVL(itemLink)
-					
+
 			local attrs = {}
 			if (SavedData.show_subtype and itemSubType ~= nil) then
 				if (itemClassId == Enum.ItemClass.Armor and itemSubClassId == 0) then
-				-- don't display Miscellaneous for rings, necks and trinkets
+					-- don't display Miscellaneous for rings, necks and trinkets
 				elseif (itemClassId == Enum.ItemClass.Armor and itemEquipLoc == "INVTYPE_CLOAK") then
-				-- don't display Cloth for cloaks
+					-- don't display Cloth for cloaks
 				else
-					if (SavedData.subtype_short_format) then 
-						table.insert(attrs, itemSubType:sub(0, 1)) 
-					else 
-						table.insert(attrs, itemSubType) 
+					if (SavedData.subtype_short_format) then
+						table.insert(attrs, itemSubType:sub(0, 1))
+					else
+						table.insert(attrs, itemSubType)
 					end
 				end
 				if (itemClassId == Enum.ItemClass.Gem and itemSubClassId == Enum.ItemArmorSubclass.Relic) then
@@ -145,16 +147,18 @@ local function Filter(self, event, message, user, ...)
 					table.insert(attrs, relicType)
 				end
 			end
-			if (SavedData.show_equiploc and itemEquipLoc ~= nil and _G[itemEquipLoc] ~= nil) then table.insert(attrs, _G[itemEquipLoc]) end
-			if (SavedData.show_ilevel and iLevel ~= nil) then 
+			if (SavedData.show_equiploc and itemEquipLoc ~= nil and _G[itemEquipLoc] ~= nil) then
+				table.insert(attrs, _G[itemEquipLoc])
+			end
+			if (SavedData.show_ilevel and iLevel ~= nil) then
 				local txt = iLevel
 				if (ItemHasSockets(itemLink)) then txt = txt .. "+S" end
 				table.insert(attrs, txt)
 			end
-			
-			local newItemName = itemName.." ("..table.concat(attrs, " ")..")"
-			local newLink = color.."|H"..itemString.."|h["..newItemName.."]|h|r"
-			
+
+			local newItemName = itemName .. " (" .. table.concat(attrs, " ") .. ")"
+			local newLink = color .. "|H" .. itemString .. "|h[" .. newItemName .. "]|h|r"
+
 			message = string.gsub(message, EscapeSearchString(itemLink), newLink)
 		end
 	end
@@ -194,49 +198,49 @@ local function EventHandler(self, event, ...)
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", Filter)
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", Filter);
 	end
-		
+
 	local panel = CreateFrame("Frame", "OptionsPanel", UIParent)
 	panel.name = "ItemLinkLevel"
 
 	local subtypeCheckBox = CreateFrame("CheckButton", "subtypeCheckBox", panel, "UICheckButtonTemplate")
-	subtypeCheckBox:SetPoint("TOPLEFT",10, -30)
+	subtypeCheckBox:SetPoint("TOPLEFT", 10, -30)
 	subtypeCheckBox:SetChecked(SavedData.show_subtype)
-	_G[subtypeCheckBox:GetName().."Text"]:SetText("Display armor/weapon type (Plate, Leather, ...)")
+	_G[subtypeCheckBox:GetName() .. "Text"]:SetText("Display armor/weapon type (Plate, Leather, ...)")
 	subtypeCheckBox:SetScript("OnClick", function(self) SavedData.show_subtype = self:GetChecked() end)
-	
+
 	local subtypeShortCheckBox = CreateFrame("CheckButton", "subtypeShortCheckBox", panel, "UICheckButtonTemplate")
-	subtypeShortCheckBox:SetPoint("TOPLEFT",30, -60)
+	subtypeShortCheckBox:SetPoint("TOPLEFT", 30, -60)
 	subtypeShortCheckBox:SetChecked(SavedData.subtype_short_format)
-	_G[subtypeShortCheckBox:GetName().."Text"]:SetText("Short format (P for plate, L for leather, ...)")
+	_G[subtypeShortCheckBox:GetName() .. "Text"]:SetText("Short format (P for plate, L for leather, ...)")
 	subtypeShortCheckBox:SetScript("OnClick", function(self) SavedData.subtype_short_format = self:GetChecked() end)
 
 	local equipLocCheckbox = CreateFrame("CheckButton", "equipLocCheckbox", panel, "UICheckButtonTemplate")
-	equipLocCheckbox:SetPoint("TOPLEFT",10, -90)
+	equipLocCheckbox:SetPoint("TOPLEFT", 10, -90)
 	equipLocCheckbox:SetChecked(SavedData.show_equiploc)
-	_G[equipLocCheckbox:GetName().."Text"]:SetText("Display equip location (Head, Trinket, ...)")
+	_G[equipLocCheckbox:GetName() .. "Text"]:SetText("Display equip location (Head, Trinket, ...)")
 	equipLocCheckbox:SetScript("OnClick", function(self) SavedData.show_equiploc = self:GetChecked() end)
 
 	local iLevelCheckbox = CreateFrame("CheckButton", "iLevelCheckbox", panel, "UICheckButtonTemplate")
-	iLevelCheckbox:SetPoint("TOPLEFT",10, -120)
+	iLevelCheckbox:SetPoint("TOPLEFT", 10, -120)
 	iLevelCheckbox:SetChecked(SavedData.show_ilevel)
-	_G[iLevelCheckbox:GetName().."Text"]:SetText("Display item level")
+	_G[iLevelCheckbox:GetName() .. "Text"]:SetText("Display item level")
 	iLevelCheckbox:SetScript("OnClick", function(self) SavedData.show_ilevel = self:GetChecked() end)
-	
+
 	local triggerLootsCheckbox = CreateFrame("CheckButton", "triggerLootsCheckbox", panel, "UICheckButtonTemplate")
-	triggerLootsCheckbox:SetPoint("TOPLEFT",10, -180)
+	triggerLootsCheckbox:SetPoint("TOPLEFT", 10, -180)
 	triggerLootsCheckbox:SetChecked(SavedData.trigger_loots)
-	_G[triggerLootsCheckbox:GetName().."Text"]:SetText("Trigger on loots (requires restart)")
+	_G[triggerLootsCheckbox:GetName() .. "Text"]:SetText("Trigger on loots (requires restart)")
 	triggerLootsCheckbox:SetScript("OnClick", function(self) SavedData.trigger_loots = self:GetChecked() end)
-	
+
 	local triggerChatCheckbox = CreateFrame("CheckButton", "triggerChatCheckbox", panel, "UICheckButtonTemplate")
-	triggerChatCheckbox:SetPoint("TOPLEFT",10, -210)
+	triggerChatCheckbox:SetPoint("TOPLEFT", 10, -210)
 	triggerChatCheckbox:SetChecked(SavedData.trigger_chat)
-	_G[triggerChatCheckbox:GetName().."Text"]:SetText("Trigger on chat messages (requires restart)")
+	_G[triggerChatCheckbox:GetName() .. "Text"]:SetText("Trigger on chat messages (requires restart)")
 	triggerChatCheckbox:SetScript("OnClick", function(self) SavedData.trigger_chat = self:GetChecked() end)
-	
+
 	local triggerQualityLabel = panel:CreateFontString("triggerQualityLabel", nil, "GameTooltipText")
 	triggerQualityLabel:SetText("Minimum trigger quality")
-	triggerQualityLabel:SetPoint("TOPLEFT",10,-260)
+	triggerQualityLabel:SetPoint("TOPLEFT", 10, -260)
 	local triggerQualityValue = panel:CreateFontString("triggerQualityValue", nil, "GameTooltipText")
 	local triggerQualitySlider = CreateFrame("Slider", "iLevelTriggerQualityValue", panel, "OptionsSliderTemplate")
 	triggerQualitySlider:SetPoint("LEFT", triggerQualityLabel, "RIGHT", 10, 0)
@@ -245,15 +249,16 @@ local function EventHandler(self, event, ...)
 	triggerQualitySlider:SetValueStep(1)
 	getglobal(triggerQualitySlider:GetName() .. 'Low'):SetText(_G["ITEM_QUALITY0_DESC"])
 	getglobal(triggerQualitySlider:GetName() .. 'High'):SetText(_G["ITEM_QUALITY6_DESC"])
-	triggerQualitySlider:SetScript("OnValueChanged", function(_, value) 
+	triggerQualitySlider:SetScript("OnValueChanged", function(_, value)
 		value = math.floor(value)
-		SavedData.trigger_quality = value 
-	    triggerQualityValue:SetText(_G["ITEM_QUALITY"..value.."_DESC"])
+		SavedData.trigger_quality = value
+		triggerQualityValue:SetText(_G["ITEM_QUALITY" .. value .. "_DESC"])
 	end)
-	
-	triggerQualityValue:SetText(_G["ITEM_QUALITY"..SavedData.trigger_quality.."_DESC"])
+
+	triggerQualityValue:SetText(_G["ITEM_QUALITY" .. SavedData.trigger_quality .. "_DESC"])
 	triggerQualityValue:SetPoint("LEFT", triggerQualitySlider, "RIGHT", 10, 0)
 
 	InterfaceOptions_AddCategory(panel)
 end
+
 frame:SetScript("OnEvent", EventHandler);
